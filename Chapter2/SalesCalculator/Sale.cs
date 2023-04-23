a128374afb5732c26bb1d12795c48fd52ffb4146
GenkiSugiyama <sgym0318marcos@gmail.com>
2023-04-23 16:28:01 +0900

    売り上げ集計プログラム初版

---
+++ Chapter2/SalesCalculator/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SalesCalculator
+{
+    //売り上げデータクラス
+    public class Sale
+    {
+        public string ShopName { get; set; }
+
+        public string ProductCategory { get; set; }
+
+        public int Amount { get; set; }
+
+    }
+}
+
---
+++ Chapter2/SalesCalculator/SalesCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesCalculator
+{
+    //売り上げ集計クラス
+    public class SalesCounter
+    {
+        private List<Sale> _sales;
+
+        public SalesCounter(List<Sale> sales)
+        {
+            this._sales = sales;
+        }
+
+        public Dictionary<string, int> GetPerStoreSales()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (var sale in _sales)
+            {
+                if (dict.ContainsKey(sale.ShopName))
+                {
+                    dict[sale.ShopName] += sale.Amount;
+                }
+                else
+                {
+                    dict[sale.ShopName] = sale.Amount;
+                }
+            }
+
+            return dict;
+        }
+    }
+}
+
